35559d2e438cb167811ccb13e90103c9e16cb525
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-01-26 17:11:23 +0000

    site

--- Site/views.py
+++ Site/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from Site.models import SiteParameters
+from Utilisateurs.models import BlogPost
 
 
 # Create your views here.
 def Index(request):
+    publication=BlogPost.objects.all()
     info_du_site = SiteParameters.objects.all()
-    return render(request, 'index.html', {'info_du_site': info_du_site})
+    return render(request, 'index.html', {'info_du_site': info_du_site, 'publication': publication})
