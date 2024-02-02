ec3f7f4574f79f493447b8d8c2d7f8ebb29bf9ef
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-02-02 16:42:56 +0000

    site

--- Site/urls.py
+++ Site/urls.py
@@ -1,10 +1,12 @@
 # Site/urls.py
 from django.urls import path
 
+from Site import views
 from Site.views import Index
 
 app_name = 'site'
 
 urlpatterns = [
     path('', Index, name='index'),
+    path('publication/<int:pk>/', views.publication_detail, name='publication_detail'),
 ]
--- Site/views.py
+++ Site/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from Site.models import SiteParameters
 from Utilisateurs.models import BlogPost
 
@@ -8,3 +8,8 @@ def Index(request):
     publication = BlogPost.objects.all()
     info_du_site = SiteParameters.objects.all()
     return render(request, 'index.html', {'info_du_site': info_du_site, 'publication': publication})
+
+
+def publication_detail(request, pk):
+    blog_posts = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'publication_detail.html', {'blog_posts': blog_posts})
