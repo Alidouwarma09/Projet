0dfddc683a6a787d7c9b91771b9189d4f4a4a029
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-03-27 16:21:12 +0000

    site

--- Projet/settings.py
+++ Projet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'django-insecure-urfsyo788$19gqs3(#%rwskvz_m2=h0l79^bzioxgqht-$n+rh'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
